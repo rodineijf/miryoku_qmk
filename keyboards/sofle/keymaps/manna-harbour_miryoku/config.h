0d18b5e6530a11758a7352202c6f7e91d2198eb8
Rodinei Fagundes <rodinei.jf@gmail.com>
2024-03-03 14:08:46 -0300

    custom

--- keyboards/sofle/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/sofle/keymaps/manna-harbour_miryoku/config.h
@@ -14,9 +14,9 @@
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
 )\
 LAYOUT(\
-XXX, XXX, XXX, XXX, XXX, XXX,                XXX, XXX, XXX, XXX, XXX, XXX,\
-XXX, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, XXX,\
-XXX, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, XXX,\
-XXX, K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, XXX,\
-          XXX, XXX, K32, K33, K34,      K35, K36, K37, XXX, XXX\
+KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV,\
+KC_TAB,  K00,  K01,  K02,  K03,  K04,                K05,  K06,  K07,  K08,  K09,  KC_BSPC,\
+KC_ESC,  K10,  K11,  K12,  K13,  K14,                K15,  K16,  K17,  K18,  K19,  KC_QUOT,\
+KC_LSFT, K20,  K21,  K22,  K23,  K24, XXX,      XXX, K25,  K26,  K27,  K28,  K29,  KC_RSFT,\
+               XXX,  XXX,  K32,  K33, K34,      K35, K36,  K37,  XXX,  XXX\
 )
