bb54016a04ed467d50b1fb51ef0a4b46f5d13c4a
Rodinei Fagundes <rodinei.jf@gmail.com>
2024-03-03 13:38:32 -0300

    custom

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,8 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE = yes
+WPM_ENABLE = yes  # Enable word per minute counter
+CONSOLE_ENABLE = yes  # Enable console for debug
